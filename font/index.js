969bcefb359a11b5429c7843b3cd225332bb94d6
Вадим <vadMoroz13@yandex.ru>
2020-06-16 20:04:24 +0300

    Работа с Dom начало

---
+++ font/index.js
@@ -0,0 +1,31 @@
+'use strict';
+alert('hi')
+const start = document.getElementById('start');
+const btnPluses = document.getElementsByTagName('button');
+const btnPlusIncome = btnPluses.querySelector('.income_add');
+const btnPlusExpenses = btnPluses.querySelector('.expenses_add');
+const depositCheck = document.querySelector('#deposit-check');
+const additionalIncomeItem = document.querySelectorAll('.additional_income-item');
+const budgetMonthValue = document.querySelector('.budget_month-value');
+const budgetDayValue = document.querySelector('.budget_day-value');
+const expensesMonthValue = document.querySelector('.expenses_month-value');
+const additionalIncomeValue = document.querySelector('.additional_income-value');
+const additionalExpensesValue = document.querySelector('.additional_expenses-value');
+const incomePeriodValue = document.querySelector('.income_period-value');
+const targetMonthValue = document.querySelector('.target_month-value');
+const inputSalarAymount = document.querySelector('.salary-amount');
+const inputIncomeTitle = document.querySelector('.income-title');
+const inputIncomeAmount = document.querySelector('.income-amount');
+const inputExpensesTitle = document.querySelector('.expenses-title');
+const inputExpensesAmount = document.querySelector('.expenses-amount');
+const inputAdditionalIncomeItem = document.querySelectorAll('.additional_income-item');
+
+const inputAdditionalExpensesItem = document.querySelector('.additional_expenses-item');
+const inputDepositCheck = document.querySelector('#deposit-check');
+const inputDepositAmount = document.querySelector('.deposit-amount');
+const inputDepositPercent = document.querySelector('.deposit-percent');
+const inputTargetAmount = document.querySelector('.target-amount');
+const inputPeriodSelect = document.querySelector('.period-select');
+ alert('hi')
+
+console.log(inputPeriodSelect);
---
+++ script/oop.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const start = document.getElementById('start');
+const btnPluses = document.getElementsByTagName('button');
+const btnPlusIncome = document.querySelector('.income_add');
+const btnPlusExpenses = document.querySelector('.expenses_add');
+const depositCheck = document.querySelector('#deposit-check');
+const additionalIncomeItem = document.querySelectorAll('.additional_income-item');
+const budgetMonthValue = document.querySelector('.budget_month-value');
+const budgetDayValue = document.querySelector('.budget_day-value');
+const expensesMonthValue = document.querySelector('.expenses_month-value');
+const additionalIncomeValue = document.querySelector('.additional_income-value');
+const additionalExpensesValue = document.querySelector('.additional_expenses-value');
+const incomePeriodValue = document.querySelector('.income_period-value');
+const targetMonthValue = document.querySelector('.target_month-value');
+const inputSalarAymount = document.querySelector('.salary-amount');
+const inputIncomeTitle = document.querySelector('.income-title');
+const inputIncomeAmount = document.querySelector('.income-amount');
+const inputExpensesTitle = document.querySelector('.expenses-title');
+const inputExpensesAmount = document.querySelector('.expenses-amount');
+const inputAdditionalIncomeItem = document.querySelectorAll('.additional_income-item');
+const inputAdditionalExpensesItem = document.querySelector('.additional_expenses-item');
+const inputDepositCheck = document.querySelector('#deposit-check');
+const inputDepositAmount = document.querySelector('.deposit-amount');
+const inputDepositPercent = document.querySelector('.deposit-percent');
+const inputTargetAmount = document.querySelector('.target-amount');
+const inputPeriodSelect = document.querySelector('.period-select');
+ 
